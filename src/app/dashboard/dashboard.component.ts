1f1d26274c8f3d9a18957331d6ac1a60c2df9053
Hugo Machado <hugomontes@hotmail.fr>
2023-08-15 10:10:51 +0200

    ngfor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,9 +13,14 @@ export class DashboardComponent implements OnInit{
   pendingtasks!: number;
   upComingProjects!: number;
   projectCost!: number;
-
   currentExpenditure!: number;
   availableFunds!: number;
+
+  clients!: string[];
+  projects!: string[];
+  years: number[] = [];
+  teamMembersSummary: any[] = [];
+  teamMembers: any[] = [];
 
   ngOnInit(): void {
     this.designation = "Chef d'équipe";
@@ -28,7 +33,58 @@ export class DashboardComponent implements OnInit{
     this.currentExpenditure=9876;
     this.availableFunds=54340;
 
+    this.clients= ["ABC Ltd", "DEF Solutions", "GHI Industries" ];
+    this.projects= ["Project A", "Project B", "Project C" ];
 
+    for(let i=2019; i>2015; i--){
+      this.years.push(i);
+    }
+
+    this.teamMembersSummary = [
+      { region: "East", teamMembersCount: 21, temporaryUnavailableMembers: 4},
+      { region: "North", teamMembersCount: 41, temporaryUnavailableMembers: 3},
+      { region: "West", teamMembersCount: 15, temporaryUnavailableMembers: 2},
+      { region: "South", teamMembersCount: 43, temporaryUnavailableMembers: 4}
+    ];
+
+    this.teamMembers = [
+      {
+        region:"East", members: [
+          {id:1, name:"Ford", status:"Avalaible"},
+          {id:2, name:"Dupond", status:"Avalaible"},
+          {id:3, name:"Frerot", status:"Busy"},
+          {id:4, name:"Lopez", status:"Avalaible"},
+
+        ]
+      },
+      {
+        region:"North", members: [
+          {id:12, name:"Ford", status:"Avalaible"},
+          {id:11, name:"Dupond", status:"Avalaible"},
+          {id:10, name:"Frerot", status:"Busy"},
+          {id:9, name:"Lopez", status:"Avalaible"},
+
+        ]
+      },
+      {
+        region:"West", members: [
+          {id:13, name:"Ford", status:"Avalaible"},
+          {id:14, name:"Dupond", status:"Avalaible"},
+          {id:15, name:"Frerot", status:"Busy"},
+          {id:16, name:"Lopez", status:"Avalaible"},
+
+        ]
+      },
+      {
+        region:"South", members: [
+          {id:1, name:"Ford", status:"Avalaible"},
+          {id:2, name:"Dupond", status:"Avalaible"},
+          {id:3, name:"Frerot", status:"Busy"},
+          {id:4, name:"Lopez", status:"Avalaible"},
+
+        ]
+      }
+    ]
 
 
   }
